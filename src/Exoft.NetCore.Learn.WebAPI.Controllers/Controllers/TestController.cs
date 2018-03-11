64c79b2ddcf36dcdc715e949ab19846d0d2c5d29
Bogdan Sakalo <bodya.sakalo@gmail.com>
2018-03-11 18:38:41 +0200

    lesson 2 in progress

---
+++ src/Exoft.NetCore.Learn.WebAPI.Controllers/Controllers/TestController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Exoft.NetCore.Learn.WebAPI.Controllers.Controllers
+{
+    [Route("api/[controller]")]
+    public class TestController : Controller
+    {
+        // GET api/test/request-headers
+        [HttpGet("request-headers")]
+        public string Get()
+        {
+            string data = "";
+            foreach (var o in Request.Headers)
+            {
+                string val = "";
+                foreach (string s in o.Value)
+                {
+                    val += s + ";";
+                }
+                data += o.Key + " : " + val + Environment.NewLine;
+            }
+            return data;
+        }
+
+        // GET api/test/200
+        [HttpGet("200")]
+        public void Get204()
+        {
+            // this method returns nothing but StatusCode 200
+        }
+    }
+}
